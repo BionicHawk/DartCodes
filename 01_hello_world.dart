139c62c1a8caf7f2c30d3e1d48a5374ed50fed94
BionicHawk <angel.manzo.rosas@gmail.com>
2023-08-14 17:15:58 -0600

    Primer commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main(){
+  // var myName = 'Angel';
+  // final String myName = 'Angel';
+  // late final String myName;
+  const String myName = 'Angel';
+
+  // myName = 'Angel';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print("La suma de 1 + 1 es ${1 + 1}");
+}
