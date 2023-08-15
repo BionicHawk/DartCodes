92459c601722b7f2bc45290c0040e9919d2e7ab7
BionicHawk <angel.manzo.rosas@gmail.com>
2023-08-14 18:03:12 -0600

    Lunes 14 Ago 2023

--- 03_mapa.dart
+++ 03_mapa.dart
@@ -1,28 +1,16 @@
-void main(List<String> args){
-
-  final pokemon = {
+void main(List<String> args) {
+  var pokemon = {
     "name": "Ditto",
     "hp": 100,
     'isAlive': true,
     'abilities': ['impostor'],
-    'sprites': {
-      1: "ditto/front.png",
-      2: "ditto/back.png"
-    }
+    'sprites': <int, String>{1: "ditto/front.png", 2: "ditto/back.png"}
   };
-  // final pokemons = {
-  //   1: "ABC",
-  //   2: "XYZ",
-  //   3: "123",
-  //   150: "AGJH"
-  // };
 
   print(pokemon);
   print('Name: ${pokemon['name']}');
   print('HP: ${pokemon['hp']}');
   print('Images: ${pokemon['sprites']}');
-  print('Images: ${ pokemon['sprites'][1] }');
-  print('Images: ${ pokemon['sprites'][2] }');
-
-  
+  print('Images: ${pokemon['sprites'] ?? [1]}');
+  print('Images: ${pokemon['sprites'] ?? [2]}');
 }
---
+++ 04_List_iterables_sets.dart
@@ -0,0 +1,11 @@
+void main() {
+  final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10];
+  print('Original List $numbers');
+  print('Original List ${numbers.length}');
+  print('Original List ${numbers[0]}');
+  print('Original List ${numbers.first}');
+  print('Original List ${numbers.last}');
+  print('Reversed List ${numbers.reversed}');
+
+  final reversedNumbers = numbers.reversed;
+}
